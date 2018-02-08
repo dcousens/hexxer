b40e29f4c1f5ec65088946ba82d1840f3590e311
Daniel Cousens <github@dcousens.com>
2018-02-08 13:35:57 +1100

    re-usability friendly

--- hexxer.cpp
+++ hexxer.cpp
@@ -1,41 +1,67 @@
+#include <array>
 #include <cstdio>
 #include <cstring>
 #include <stdexcept>
 
 #include "hexxer.hpp"
 
-int main (int argc, char** argv) {
-	char hbuf[2] = {};
-	unsigned char bbuf[1] = {};
+#define MAX_WRITE_OUT 4096
 
-	// decode?
+auto encode () {
+	std::array<unsigned char, MAX_WRITE_OUT / 2> in;
+	std::array<char, MAX_WRITE_OUT> out;
+
+	while (true) {
+		// read until EOF
+		const auto read = fread(in.data(), 1, in.size(), stdin);
+		if (read == 0) break;
+
+		for (size_t i = 0; i < read; ++i) {
+			const auto byte = in[i];
+			out[i << 1] = hexxer::encodeFirst(byte);
+			out[(i << 1) + 1] = hexxer::encodeSecond(byte);
+		}
+
+		if (!fwrite(out.data(), read * 2, 1, stdout)) return 1;
+	}
+
+	return 0;
+}
+
+auto decode () {
+	std::array<char, MAX_WRITE_OUT * 2> in;
+	std::array<unsigned char, MAX_WRITE_OUT> out;
+
+	while (true) {
+		// read until EOF
+		const auto read = fread(in.data(), 1, in.size(), stdin);
+		if (!read) break;
+		if (read % 2 != 0) throw std::domain_error("Odd number of characters");
+
+		for (size_t i = 0; i < read; i += 2) {
+			const auto a = in[i];
+			const auto b = in[i + 1];
+
+			const auto byte = hexxer::decode(a, b);
+			if (byte > 0xff) throw std::domain_error("Invalid hex character");
+
+			out[i >> 1] = static_cast<unsigned char>(byte);
+		}
+
+		if (!fwrite(out.data(), read, 1, stdout)) return 1;
+	}
+
+	return 0;
+}
+
+int main (int argc, char** argv) {
 	if (argc > 1) {
 		if (strncmp(argv[1], "-d", 2) != 0 && strncmp(argv[1], "--decode", 8) != 0) {
 			throw std::invalid_argument("Unknown argument");
 		}
 
-		while (true) {
-			const auto read = fread(hbuf, sizeof(hbuf), 1, stdin);
-
-			// EOF?
-			if (read == 0) break;
-			if (!hex_decode(bbuf, hbuf, 2)) throw std::domain_error("Invalid hex character");
-
-			fwrite(bbuf, sizeof(bbuf), 1, stdout);
-		}
-
-	// encode
-	} else {
-		while (true) {
-			const auto read = fread(bbuf, sizeof(bbuf), 1, stdin);
-
-			// EOF?
-			if (read == 0) break;
-			if (!hex_encode(hbuf, bbuf, 1)) return 1;
-
-			fwrite(hbuf, sizeof(hbuf), 1, stdout);
-		}
+		return decode();
 	}
 
-	return 0;
+	return encode();
 }
