b218105054f6e8191547192f1eca8fee990b05b5
Daniel Cousens <dcousens@users.noreply.github.com>
2022-09-09 11:45:35 +1000

    const switcharoo

--- hexxer.hpp
+++ hexxer.hpp
@@ -1,8 +1,31 @@
+// MIT License
+//
+// Copyright (c) 2022 Daniel Cousens
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
+#include <cstdint>
 
 namespace hexxer {
-	static const char HEX_ALPHABET[] = "0123456789abcdef";
-	static const int HEX_TABLE[] = {
+	static char const HEX_ALPHABET[] = "0123456789abcdef";
+	static int const HEX_TABLE[] = {
 		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
 		0,1,2,3,4,5,6,7,8,9, // 0-9
 		255,255,255,255,255,255,255,
@@ -12,17 +35,17 @@ namespace hexxer {
 		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255
 	};
 
-	inline auto encodeFirst (const unsigned char x) {
+	inline char encodeFirst (uint8_t const x) {
 		return HEX_ALPHABET[x >> 4];
 	}
 
-	inline auto encodeSecond (const unsigned char x) {
+	inline char encodeSecond (uint8_t const x) {
 		return HEX_ALPHABET[x & 0x0f];
 	}
 
-	inline auto decode (const char a, const char b) {
-		const auto ia = HEX_TABLE[static_cast<unsigned char>(a)];
-		const auto ib = HEX_TABLE[static_cast<unsigned char>(b)];
+	inline uint16_t decode (char const a, char const b) {
+		auto const ia = HEX_TABLE[static_cast<unsigned char>(a)];
+		auto const ib = HEX_TABLE[static_cast<unsigned char>(b)];
 		if (ia == 255) return 0x100;
 		if (ib == 255) return 0x100;
 
